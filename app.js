1b9df53147823ce6d1e8d3614b2ab721476fe21d
Yassine AZZOUT <92bondstreet@gmail.com>
2014-10-01 08:30:38 +0200

    Update and migrate to Express 4.x

--- app.js
+++ app.js
@@ -5,15 +5,15 @@
 
 		// bind for connections
 		var server = require('./lib/server');
-		
+
 		// Setup routes
 		var routes = require('./lib/routes');
 		routes.Setup(server);
 
-		// Listen for connections 
+		// Listen for connections
 		var port = 3000;
 		server.listen(port);
 
-		debug("Express 3.x server listening on port %d in %s mode", port, process.env.NODE_ENV);
+		debug("Express 4.x server listening on port %d in %s mode", port, process.env.NODE_ENV);
 	}
 )();
